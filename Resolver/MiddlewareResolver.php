1c90eaed50336cfd80b182a8449aac1959f51b2c
Antonio Ramirez <amigo.cobos@gmail.com>
2017-02-11 20:40:10 +0100

    re #3 added queue manager ~done

---
+++ Resolver/MiddlewareResolver.php
@@ -0,0 +1,38 @@
+<?php
+namespace Altair\Middleware\Resolver;
+
+use Altair\Container\Container;
+use Altair\Middleware\Contracts\MiddlewareInterface;
+use Altair\Middleware\Contracts\ResolverInterface;
+
+class MiddlewareResolver implements ResolverInterface
+{
+    /**
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * MiddlewareResolver constructor.
+     *
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param $entry
+     *
+     * @return MiddlewareInterface
+     */
+    public function __invoke($entry): MiddlewareInterface
+    {
+        if (is_object($entry)) {
+            return $entry;
+        }
+
+        return $this->container->make($entry);
+    }
+}
